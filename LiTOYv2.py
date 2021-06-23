ef471168d9e83dbebe6f7267915ce52c34577e3b
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-06-23 11:22:23 +0200

    refactor: creates new version from scratch

---
+++ LiTOYv2.py
@@ -0,0 +1,72 @@
+# TODOS :
+# * respect pep8
+# * use type hints from the beginning
+# * use docstrings everywhere
+
+
+
+
+
+
+#===============================================================================
+# Summary of each section
+# 0. license
+# 1. user settings
+# 2. fonctions etc 
+# 3. main routine
+
+
+
+#===============================================================================
+# 1. User defined settings
+
+#===============================================================================
+# 2. Initialization, definitions etc
+# imports :
+import pandas as pd
+import argparse
+import logging as lg
+
+
+# check if logging file already exists
+# check if database already exists, if not create it
+
+# classes :
+class LiTOYClass():
+    "Defines the methods to interact with the database using panda"
+    def self.__init__():
+        pass
+    def _load_from_file():
+        pass
+    def _get_tags():
+        pass
+
+    self.load_from_file = _load_from_file()
+    self.get_tags = _get_tags()
+
+class entryClass():
+    "Defines the methods to interact with one entry of the database"
+    def self.__init__():
+        pass
+
+
+db=LiTOYClass()
+entry=entryClass(id)
+
+
+# arguments :
+
+#===============================================================================
+# 3. Main routine
+
+
+if __name__ == "__main__":
+    pass
+
+
+
+
+
+
+
+
