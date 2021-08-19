1fdb4dd00185130656a3130c86e583d4fa871ac4
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-08-19 12:13:33 +0200

    style: completion is now called progress

--- LiTOY.py
+++ LiTOY.py
@@ -473,13 +473,13 @@ def show_stats(df, printing=True):
             print(table)
             print("")
             print(table2)
-            print(f"Completion (lower is better):{round(completion_score, 3):>20}")
+            print(f"Progress (lower is better):{round(completion_score, 3):>20}")
         else:
             table.border = False
             table2.border = False
             log_(table)
             log_(table2)
-            log_(f"Completion (lower is better):{round(completion_score, 3):>20}")
+            log_(f"Progress (lower is better):{round(completion_score, 3):>20}")
     except StatisticsError as e:
         log_(f"Not enough data points! {e}", False)
         raise SystemExit()
